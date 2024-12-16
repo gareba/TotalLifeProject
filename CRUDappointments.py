bd3ccc99178f1eee4766c4b7130317e99014b454
gareba <90716449+gareba@users.noreply.github.com>
2024-12-16 09:52:15 -0700

    updated but did not test CRUDappointments

--- CRUDappointments.py
+++ CRUDappointments.py
@@ -5,29 +5,29 @@ import requests # for the api
 #CRUD FUNCS FOR appointments
 
 #CREATE 
-def create_appointment():
+def create_appointment(NPInum, pid, date,time,location,duration):
 
     # if everything is all fine and dandy, go ahead and add to the database. 
-    dbname = "part1db.db"
+    dbname = "part1DB.db"
     conn = sqlite3.connect(dbname)
     c = conn.cursor()
-    statement = '''INSERT INTO appointments (db parameters here ) VALUES (?,?,?,?,?,?);'''
-    data = () #tuple of all args here
+    statement = '''INSERT INTO appointments (NPInum, patient, date, time, location, duration) VALUES (?,?,?,?,?,?);'''
+    data = (NPInum, pid, date,time,location,duration) #tuple of all args here
     c.execute(statement,data)
     conn.commit()
     return
 
 #READ 
-def read_clinician():
+def read_appointment(NPInum, pid):
     # need to do: implement functionality for filtering by times 
-    dbname = "part1db.db"
+    dbname = "part1DB.db"
     conn = sqlite3.connect(dbname)
     c = conn.cursor()
-    c.execute('''SELECT FROM appointments where NID == clinician info and pid == patient info;''')
+    c.execute('''SELECT FROM appointments where NPInum == :clinician and pid == :patient info;''',{"clinician":NPInum, "patient": pid})
     appointments = c.fetchall()
     success = True
     for appointment in appointment:
-            print("display appointment information ")
+            print(appointment)
             
     conn.commit()
     conn.close()
@@ -35,13 +35,21 @@ def read_clinician():
 
 
 #UPDATE 
-def update_appointment():
-        dbname = "part1db.db"
+def update_appointment(NPInum, pid, date, time, dictionary):
+        data = []
+        dbname = "part1DB.db"
+        statement = '''UPDATE appointments SET '''
+        for field in ("location","duration"):
+              if field in dictionary:
+                    data.append(dictionary[field])
+                    statement = statement + field + ''' = ?, '''
+
+        statement = statement[:len(statement) -2]
+        statement = statement + ''' where pid == ? and NPInum == ? and date == ? and time == ?'''
+        data.append(pid,NPInum, date,time)
         conn = sqlite3.connect(dbname)
         c = conn.cursor()
-        c.execute('''UPDATE  appointment 
-                  SET f1 = v1, etc etc
-                  where NID == clinician info and patient and time and date;''')
+        c.execute(statement, data)
         success = True        
         conn.commit()
         conn.close()
@@ -49,11 +57,11 @@ def update_appointment():
 
 
 #DELETE 
-def delete_appointment():
+def delete_appointment(NPInum, pid, date, time):
         dbname = "part1db.db"
         conn = sqlite3.connect(dbname)
         c = conn.cursor()
-        c.execute('''DELETE FROM appointment where NID == clinician info;''')
+        c.execute('''DELETE FROM appointments where NPInum == ? and pid == ? and date == ? and time == ?;''',(NPInum,pid,date,time))
         success = True        
         conn.commit()
         conn.close()
